4a0703c3ebc8e6b9acf559f42276f1b61017e32a
prchen <prchen4git@aliyun.com>
2018-06-16 10:35:51 +0800

    Remove useless test config @Import

--- src/test/java/test/TestApp.java
+++ src/test/java/test/TestApp.java
@@ -1,14 +1,11 @@
 package test;
 
-import com.github.prchen.antares.starter.AntaresAutoConfiguration;
 import com.github.prchen.antares.starter.AntaresScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @AntaresScan
 @SpringBootApplication
-@Import(AntaresAutoConfiguration.class)
 public class TestApp {
 
     public static void main(String[] args) {
